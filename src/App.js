3d30f466ae912b449363bb5bcaa5d299629a5531
tonykung <tonykung06@gmail.com>
2016-03-27 20:45:45 +0800

    nested routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {Router, Route, Link, browserHistory} from 'react-router';
+import {Router, Route, Link, browserHistory, hashHistory} from 'react-router';
 
-const Home = () => <div><h1>Home</h1><Links /></div>;
-const About = () => <div><h1>About</h1><Links /></div>;
-const Contact = () => <div><h1>Contact</h1><Links /></div>;
+const Home = (props) => <div><h1>Home</h1><Links />{props.children}</div>;
+const About = (props) => <div><h1>About</h1>{props.children}</div>;
+const Contact = () => <div><h1>Contact</h1></div>;
 
 const Links = () => (
 	<nav>
-		<div><Link to="/">Home</Link></div>
-		<div><Link to="/about">About</Link></div>
-		<div><Link to="/contact">Contact</Link></div>
+		<Link activeClassName="active-nav-link" activeStyle={{color: 'green'}} to="/">Home</Link>
+		<Link activeClassName="active-nav-link" activeStyle={{color: 'green'}} to="/about">About</Link>
+		<Link activeClassName="active-nav-link" activeStyle={{color: 'green'}} to="/about/contact">Contact</Link>
 	</nav>
 );
 
@@ -17,10 +17,12 @@ const Links = () => (
 class App extends React.Component {
 	render() {
 		return (
-			<Router history={browserHistory}>
-				<Route path="/" component={Home} />
-				<Route path="/About" component={About} />
-				<Route path="/contact" component={Contact} />
+			<Router history={hashHistory}>
+				<Route path="/" component={Home}>
+					<Route path="about" component={About}>
+						<Route path="contact" component={Contact} />
+					</Route>
+				</Route>
 			</Router>
 		);
 	}
