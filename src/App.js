61449c303e3b0d0cafd568f0ae5c06a42cd00c2d
tonykung <tonykung06@gmail.com>
2016-03-27 21:09:58 +0800

    route params

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const DefaultComponent = () => <div>no sub-route, this is Index Route</div>;
 
 const Home = (props) => <div><h1>Home</h1><Links />{props.children}</div>;
 const About = (props) => <div><h1>About</h1>{props.children}</div>;
-const Contact = (props) => <div><h1>Contact</h1></div>;
+const Contact = (props) => <div><h1>Contact</h1>{props.params.message} {props.children}</div>;
 
 const Links = () => (
 	<nav>
@@ -23,7 +23,9 @@ class App extends React.Component {
 				<Route path="/" component={Home}>
 					<Route path="about" component={About}>
 						<IndexRoute component={DefaultComponent} />
-						<Route path="contact" component={Contact} />
+						<Route path="contact/(:message)" component={Contact}>
+							<IndexRoute component={DefaultComponent} />
+						</Route>
 					</Route>
 				</Route>
 			</Router>
